cb719fc8ca9dba21170a5380d3d9a2c2def7a921
bhavyab-042 <73235775+bhavyab-042@users.noreply.github.com>
2023-02-20 09:29:28 +0530

    Update Quadrilateral.cpp

--- Polygons/Quadrilateral.cpp
+++ Polygons/Quadrilateral.cpp
@@ -1,1 +1,31 @@
+#include<Windows.h>
+#include<gl/glut.h>
+
+void display1()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	glPointSize(50);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
+
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("Quadrilateral");
+	glutInitWindowSize(620, 620);
+	glutInitWindowPosition(50, 50);
+	glutDisplayFunc(display1);
+	glutMainLoop();
+	return 0;
+}
 
